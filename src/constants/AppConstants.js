3d1cf35d9d1c6d4d7d6518a47932cf199b2e90c2
Anand Gupta <aanand.gupta03@gmail.com>
2020-07-27 03:15:59 +0530

    Added Page Title

--- src/constants/AppConstants.js
+++
@@ -1,9 +0,0 @@
-export const INCREASE_COUNTER = 'Increase Counter';
-export const DECREASE_COUNTER = 'Decrease Counter';
-
-export const requestObject = {
-  url: '',
-  params: {},
-  data: {},
-  config: {},
-};
